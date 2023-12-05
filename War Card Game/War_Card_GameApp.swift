d0596316b03191600fd0d0091514adcde5207643
umesh.tikhe.cons <umesh.tikhe.cons@kuhoo.com>
2023-12-05 23:04:02 +0530

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Umesh tikhe on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
