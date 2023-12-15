5c0ae27ad97d80ae2a85d41173fab9a99d64ab1f
umesh.tikhe.cons <umesh.tikhe.cons@kuhoo.com>
2023-12-15 19:58:24 +0530

    war card game finish

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -6,16 +6,66 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    @State var playerCard = "back"
+    @State var cpuCard = "back"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Image("background-plain")
+            
+            VStack(spacing:60){
+                Image("logo")
+                    .renderingMode(.original)
+                    .aspectRatio(contentMode: .fit)
+                HStack(spacing:60){
+                    Image(playerCard)
+                    Image(cpuCard)
+                }
+                Button{
+                   deal()
+                }label: {
+                    Image("button")
+                }
+                HStack(spacing: 150){
+                    VStack{
+                        Text("Player")
+                        Text("\(playerScore)")
+                    }
+                    VStack{
+                        Text("CPU")
+                        Text("\(cpuScore)")
+                    }
+                }.font(.title).foregroundColor(.white).padding()
+            }
         }
-        .padding()
+        
+    }
+    func generateRandomNumber() -> Int {
+        let lowerBound = 2
+        let upperBound = 14
+        
+        let randomNumber = Int(arc4random_uniform(UInt32(upperBound - lowerBound + 1))) + lowerBound
+        return randomNumber
+    }
+    func deal(){
+        
+        let playerDeal = Int.random(in: 2...14)
+        let cpuDeal = Int.random(in: 2...14)
+        playerCard = "card" + String(playerDeal)
+        
+        cpuCard = "card" + String(cpuDeal)
+      
+    
+        if(playerDeal > cpuDeal){
+            playerScore += 1
+        }else{
+            cpuScore += 1
+        }
+       
     }
 }
 
